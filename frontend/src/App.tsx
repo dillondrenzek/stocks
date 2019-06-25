b7a87303e28d2105ba66b8827add6196024bd1cd
Dillon Drenzek <dillon@drenzek.com>
2019-06-24 22:52:20 -0700

    [React] Quote Table displays a table of mock quotes  (#4)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { QuoteTable } from './components/quote-table/quote-table'
 import './App.scss';
+
+import Quotes from './get-quotes.json';
 
 export class App extends React.Component {
   render() {
     return (
       <div>
-        Home
+        <QuoteTable
+          quotes={Quotes.results}
+        />
       </div>
     );
   }
