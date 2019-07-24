6a1b37112e5bbfce18c7da9bd7c53dc25e8da459
Dillon Drenzek <dillon@drenzek.com>
2019-07-23 22:40:03 -0700

    update trade model

--- db/trade/Trade.spec.ts
+++ db/trade/Trade.spec.ts
@@ -1,47 +1,8 @@
-import chai from 'chai';
-const expect = chai.expect;
+import chai, { expect } from 'chai';
 import mongoose from 'mongoose';
-require('sinon-mongoose');
+import { withDb } from '../../spec/helpers/db-connect';
+import { Trade } from './Trade';
 
-// Importing our todo model for our unit testing.
-import {Trade} from './Trade';
+// xdescribe('Trade', withDb(() => {
 
-// describe('Trade', () => {
-//     let conn;
-//     let db;
-
-//     beforeEach(() => {
-//         conn = mongoose.connect('mongodb://localhost:27017/stocks-test', {
-//             useNewUrlParser: true
-//         });
-//         db = mongoose.connection;
-//         db.on('error', console.error.bind(console, 'connection error'));
-//         db.once('open', function () {
-//             done();
-//         });
-//     });
-
-//     afterEach((done) => {
-//         db.db.dropDatabase(function () {
-//             db.close(done);
-//         });
-//     });
-
-//     xit ('gets Trades', (done) => {
-//         const TradeMock = sinon.mock(Trade);
-//         const expectedResult = { status: true, todo: [] };
-//         TradeMock.expects('find').yields(null, expectedResult);
-//         Trade.find(function(err, result) {
-//             TradeMock.verify();
-//             TradeMock.restore();
-//             expect(result.status).to.be.true;
-//             done();
-//         });
-//     });
-
-//     xit ('gets a Trade by id', () => {});
-//     xit ('creates a Trade', () => {});
-//     xit ('updates a Trade', () => {});
-//     xit ('deletes a Trade by id', () => {});
-
-// });
+// }));
--- db/trade/Trade.ts
+++ db/trade/Trade.ts
@@ -1,34 +1,26 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import { IPortfolioDocument } from '../portfolio/Portfolio';
 
 export interface ITradeDocument extends mongoose.Document {
-    date: Date;
-    portfolioId: IPortfolioDocument['_id'];
-    price: number;
-    quantity: number;
-    side: 'buy' | 'sell';
-    symbol: string;
-    addToPortfolio ?: (id: IPortfolioDocument['_id']) => void;
+  price: number;
+  quantity: number;
+  side: 'buy' | 'sell';
+  symbol: string;
+  timestamp: Date;
 }
 
 export interface ITradeModel extends mongoose.Model<ITradeDocument> {
-    // findBySymbol?: (symbol: string) => mongoose.Model<ITradeDocument>;
+  // findBySymbol?: (symbol: string) => mongoose.Model<ITradeDocument>;
 }
 
 // Schema
 const tradeSchema = new mongoose.Schema<ITradeDocument>({
-    date: Date,
-    portfolioId: String,
-    price: Number,
-    quantity: Number,
-    side: String, // 'buy' or 'sell'
-    symbol: String,
+  price: Number,
+  quantity: Number,
+  side: String, // 'buy' or 'sell'
+  symbol: String,
+  timestamp: Date,
 });
-
-tradeSchema.methods.addToPortfolio = async function(id: IPortfolioDocument['_id']) {
-    this.portfolioId = id;
-    await this.save();
-};
 
 // Export
 export const Trade = mongoose.model<ITradeDocument>('Trade', tradeSchema);
