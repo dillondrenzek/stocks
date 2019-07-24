f93443a4987be9a78cdec90a72ae060dea772e20
Dillon Drenzek <dillon@drenzek.com>
2019-07-23 22:59:08 -0700

    Create StockTrade and OptionTrade models

--- db/trade/Trade.ts
+++ db/trade/Trade.ts
@@ -1,26 +1,53 @@
 import mongoose, { Schema } from 'mongoose';
-import { IPortfolioDocument } from '../portfolio/Portfolio';
+import { BuyOrSell, StockOrOption } from '../types';
 
 export interface ITradeDocument extends mongoose.Document {
   price: number;
   quantity: number;
-  side: 'buy' | 'sell';
+  side: BuyOrSell;
   symbol: string;
   timestamp: Date;
+  type: StockOrOption;
 }
 
-export interface ITradeModel extends mongoose.Model<ITradeDocument> {
-  // findBySymbol?: (symbol: string) => mongoose.Model<ITradeDocument>;
+export interface IStockTradeDocument extends ITradeDocument {
+  type: 'stock';
 }
 
-// Schema
-const tradeSchema = new mongoose.Schema<ITradeDocument>({
+export interface IOptionTradeDocument extends ITradeDocument {
+  callPut: 'call' | 'put';
+  expDate: Date;
+  openClose: 'open' | 'close';
+  strikePrice: number;
+  type: 'option';
+}
+
+// Stock Trade
+
+const stockTradeSchema = new mongoose.Schema<IStockTradeDocument>({
   price: Number,
   quantity: Number,
   side: String, // 'buy' or 'sell'
   symbol: String,
   timestamp: Date,
+  type: String,
 });
 
-// Export
-export const Trade = mongoose.model<ITradeDocument>('Trade', tradeSchema);
+export const StockTrade = mongoose.model<IStockTradeDocument>('StockTrade', stockTradeSchema);
+
+// Option Trade
+
+const optionTradeSchema = new mongoose.Schema<IOptionTradeDocument>({
+  callPut: String,
+  expDate: Date,
+  openClose: String,
+  price: Number,
+  quantity: Number,
+  side: String, // 'buy' or 'sell'
+  strikePrice: Number,
+  symbol: String,
+  timestamp: Date,
+  type: String,
+});
+
+export const OptionTrade = mongoose.model<IOptionTradeDocument>('OptionTrade', optionTradeSchema);
