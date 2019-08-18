27fc3e6f12fd152a188394f11e04f9dc7ceb8b7c
Dillon Drenzek <dillon@drenzek.com>
2019-08-17 19:24:47 -0700

    move spec folder, tests run again

--- src/controllers/PortfolioController.spec.ts
+++ src/controllers/PortfolioController.spec.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 // import mongoose from 'mongoose';
 import * as DB from '../db';
 // import { getMockDateString } from '../spec/helpers/date';
-import { withDb } from '../../spec/helpers/db-connect';
+import { withDb } from '../spec/helpers/db-connect';
 import { PortfolioController } from './PortfolioController';
 import { Portfolio, Trade, StockTrade } from '../types';
 import { defaultStockTrade } from '../db/trade';
