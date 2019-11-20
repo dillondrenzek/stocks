10b29ec30eecaa57d192ed344c8e98d20fe693f4
Dillon Drenzek <dillon@drenzek.com>
2019-11-20 00:04:32 -0800

    fix up

--- src/db/robinhood/AccountActivityItem.ts
+++ src/db/robinhood/AccountActivityItem.ts
@@ -1,37 +1,29 @@
 import mongoose, { SchemaTypes } from 'mongoose';
 
 export interface AccountActivityItem {
-  // "DESCRIPTION": "SPY 10/18/2019 Put $286.00", 
-  description: string | Object;
-  // "SYMBOL": "SPY", 
-  symbol: string;
-  // "ACCT TYPE": "Margin", 
   accountType: string;
-  // "TRANSACTION": "BTC", 
-  transactionType: string;
-  // "DATE": "10/01/2019", 
-  date: string; // MM/DD/YYYY
-  // "QTY": "1",
-  qty: number;
-  // "PRICE": "$1.56", 
-  price: number;
-  // "DEBIT": "$156.00" 
-  debit?: number;
   credit?: number
+  date: string; // MM/DD/YYYY
+  debit?: number;
+  description: string | Object; // different based on type
+  price: number;
+  qty: number;
+  symbol: string;
+  transactionType: string;
 }
 
 export type AccountActivityItemDocument = AccountActivityItem & mongoose.Document;
 
 const accountActivityItemSchema = new mongoose.Schema<AccountActivityItemDocument>({
-  description: SchemaTypes.Mixed,
-  symbol: SchemaTypes.String,
   accountType: SchemaTypes.String,
-  transactionType: SchemaTypes.String,
+  credit: SchemaTypes.Number,
   date: SchemaTypes.String,
-  qty: SchemaTypes.Number,
-  price: SchemaTypes.Number,
   debit: SchemaTypes.Number,
-  credit: SchemaTypes.Number
+  description: SchemaTypes.Mixed,
+  price: SchemaTypes.Number,
+  qty: SchemaTypes.Number,
+  symbol: SchemaTypes.String,
+  transactionType: SchemaTypes.String,
 });
 
 export const AccountActivityItem = mongoose.model<AccountActivityItemDocument>('AccountActivityItem', accountActivityItemSchema);
--- src/db/robinhood/PortfolioSummaryItem.ts
+++ src/db/robinhood/PortfolioSummaryItem.ts
@@ -1,43 +1,33 @@
 import mongoose, { SchemaTypes } from 'mongoose';
 
 export interface PortfolioSummaryItem {
-  // "EQUITIES/OPTIONS": "AdobeEstimated Yield: 0.00%", 
-  equitiesOptions: string;
-  yieldPercent: number;
-  // "SYM/CUSIP": "ADBE", 
-  symbol: string;
-  // "ACCT TYPE": "Margin", 
   accountType: string;
-  // "QTY": "1", 
-  qty: number;
-  // "PRICE": "$277.93", 
-  price: number;
-  // "MKT VALUE": "$277.93", 
-  mktValue: number;
-  // "LAST PERIOD'S MKT VALUE": "$276.25", 
-  mktValueLastPeriod: number;
-  // "% CHANGE": "0.61%", 
-  changePercent: number;
-  // "EST. ANNUAL INCOME": "$0.00", 
   annualIncome: number; // estimated
-  // "% OF TOTAL PORTFOLIO": "1.87%"
+  changePercent: number;
+  equitiesOptions: string;
+  mktValue: number;
+  mktValueLastPeriod: number;
   portfolioPercent: number;
+  price: number;
+  qty: number;
+  symbol: string;
+  yieldPercent: number; 
 }
 
 export type PortfolioSummaryItemDocument = PortfolioSummaryItem & mongoose.Document;
 
 const portfolioSummaryItemSchema = new mongoose.Schema<PortfolioSummaryItemDocument>({
-  equitiesOptions: SchemaTypes.String,
-  yieldPercent: SchemaTypes.Number,
-  symbol: SchemaTypes.String,
   accountType: SchemaTypes.String,
-  qty: SchemaTypes.Number,
-  price: SchemaTypes.Number,
+  annualIncome: SchemaTypes.Number,
+  changePercent: SchemaTypes.Number,
+  equitiesOptions: SchemaTypes.String,
   mktValue: SchemaTypes.Number,
   mktValueLastPeriod: SchemaTypes.Number,
-  changePercent: SchemaTypes.Number,
-  annualIncome: SchemaTypes.Number,
   portfolioPercent: SchemaTypes.Number,
+  price: SchemaTypes.Number,
+  qty: SchemaTypes.Number,
+  symbol: SchemaTypes.String,
+  yieldPercent: SchemaTypes.Number,
 });
 
 export const PortfolioSummaryItem = mongoose.model<PortfolioSummaryItemDocument>('PortfolioSummaryItem', portfolioSummaryItemSchema);
