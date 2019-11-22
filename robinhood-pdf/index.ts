5f6e4147d22dd93dd949c92997fef78d515677b5
Dillon Drenzek <dillon@drenzek.com>
2019-11-21 23:16:36 -0800

    can drop, save, print with robinhood cli

--- robinhood-pdf/index.ts
+++ robinhood-pdf/index.ts
@@ -1,6 +1,7 @@
 import { loadEnv } from './load-env';
 loadEnv();
 
+import { resolve } from 'path';
 import { readRobinhoodPdf } from './pdf';
 import { saveRobinhoodPdf, dropRobinhoodPdfDb } from './db';
 
@@ -12,47 +13,66 @@ if (!process.argv[2]) {
   throw new Error('No argv 2');
 }
 
-switch (process.argv[2]) {
-  case 'drop':
-    break;
-  case 'read':
-    break;
+const resolveFileName = (path: string) => {
+  return resolve(__dirname, path);
 }
 
+switch (process.argv[2]) {
 
-
-if (process.argv[2] === 'drop-robinhood-db') {
-
-  // drop all the robinhood models
-  dropRobinhoodPdfDb()
+  case 'dropDb':
+    // drop all the robinhood models
+    dropRobinhoodPdfDb()
     .then(() => {
       console.info('Successfully dropped Robinhood database');
       process.exit();
     });
-} else {
+    break;
 
-  const filename = process.argv[2];
+  case 'save': {
+    const filename = resolveFileName(process.argv[3]);
+    readRobinhoodPdf(filename)
+      .then((data) => {
+        // console.log('page data', data);
+        console.info('Successfully read Robinhood PDF');
+    
+        console.info('---Begin saving Robinhood PDF...\n');
+        saveRobinhoodPdf(data)
+          .then(() => {
+            // data saved
+            console.info('...Successfully saved Robinhood PDF---');
+            process.exit();
+          })
+          .catch((err) => {
+            console.error('FAILED to save Robinhood PDF');
+            console.error('Error:', err);
+          });
+    
+      });
+    break;
+  }
+
+  case 'print': {
+    const filename = resolveFileName(process.argv[3]);
+    console.info('Begin reading Robinhood PDF:\n', filename);
+    readRobinhoodPdf(filename)
+      .then((data) => {
+        console.info('Successfully read Robinhood PDF');
+        console.log(data);
+      });   
+    break;
+  }
+
+  default:
+    const filename = resolveFileName(process.argv[3]);
+    console.info('Begin reading Robinhood PDF:\n', filename);
+    readRobinhoodPdf(filename)
+      .then((data) => {
+        // console.log('page data', data);
+        console.info('Successfully read Robinhood PDF');
+      });
+    break;
+
   
-  // read robinhood pdf from file system
-  console.info('Begin reading Robinhood PDF:\n', filename);
-  readRobinhoodPdf(filename)
-    .then((data) => {
-      // console.log('page data', data);
-      console.info('Successfully read Robinhood PDF');
-  
-      console.info('---Begin saving Robinhood PDF...\n');
-      saveRobinhoodPdf(data)
-        .then(() => {
-          // data saved
-          console.info('...Successfully saved Robinhood PDF---');
-          process.exit();
-        })
-        .catch((err) => {
-          console.error('FAILED to save Robinhood PDF');
-          console.error('Error:', err);
-        });
-  
-    });
 }
 
 
