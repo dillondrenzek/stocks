c75f4abd4bde16e5795613b69387ef8d70ccbc17
Dillon Drenzek <dillon@drenzek.com>
2019-11-27 00:33:24 -0800

    finished moving server and api

--- server/api/index.ts
+++ server/api/index.ts
@@ -4,16 +4,7 @@ import portfoliosApi from './portfolios';
 
 const router = express();
 
-// Middleware
-router.use(Logger.logRequests);
-router.use(accessControlAllowOrigin('*'));
-
 // Router
 router.use('/portfolios', portfoliosApi);
-
-router.use('/', (req, res) => {
-  console.log('Hit / route');
-  res.send('Hit /api');
-});
 
 export default router;
--- server/index.ts
+++ server/index.ts
@@ -2,22 +2,26 @@ import { loadEnv } from './load-env';
 loadEnv('../.env');
 
 import express from 'express';
+import * as DB from '../db';
 import { Logger, accessControlAllowOrigin } from './util';
 import api from './api';
 
 const server = express();
 const port = process.env.PORT;
+const mongodbUrl = process.env.MONGODB_URL
 
 // Middleware
 server.use(Logger.logRequests);
 server.use(accessControlAllowOrigin('*'));
 
+// Database connection
+if (!mongodbUrl) {
+  throw new Error('MongoDB Url: ' + mongodbUrl);
+}
+DB.connectDb(mongodbUrl);
 
 // Routes
 server.use('/api', api);
-server.use('/', (req, res) => {
-  res.send('Index route 5');
-});
 
 
 if(!port) {
