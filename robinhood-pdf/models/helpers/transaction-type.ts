e1435a6dd7c0c5614fcc89820b4d73ecbf19ea7c
Dillon Drenzek <dillon@drenzek.com>
2019-12-19 22:47:34 -0700

    handle new TransactionType T/A

--- robinhood-pdf/models/helpers/transaction-type.ts
+++ robinhood-pdf/models/helpers/transaction-type.ts
@@ -7,12 +7,16 @@ export enum TransactionType {
   BTO = 'BTO',
   STO = 'STO',
   ACH = 'ACH', // 
+  TA = 'T/A',
 }
 
 export function isTransactionType(key: string): key is TransactionType {
-  return Object.keys(TransactionType).includes(key);
+  return Object.keys(TransactionType).includes(key) || key === 'T/A';
 }
 
 export function getTransactionType(data: string): TransactionType {
+  if (data === 'T/A') {
+    return TransactionType.TA;
+  }
   return isTransactionType(data) ? TransactionType[data] : null;
 }
