e0fd667c97440fff08b73f760b5118b62550df4d
Dillon Drenzek <dillon@drenzek.com>
2019-12-24 23:31:29 -0700

    fix database tests

---
+++ db/robinhood-pdf/PdfImport.spec.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import { withDb } from '../../spec/helpers/db-connect';
+
+describe('PdfImport', withDb(() => {
+
+  describe('- delete by id', () => {
+    xit(': Deletes the child portfolio summary items');
+    xit(': Deletes the child account activity items');
+    xit(': Deletes the PdfImport instance');
+  });
+
+}));
